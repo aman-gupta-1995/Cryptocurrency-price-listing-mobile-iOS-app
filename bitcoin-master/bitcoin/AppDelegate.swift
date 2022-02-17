e85f54755cedb89e796f9483c80f1ef87bb0bfc9
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-02-18 00:07:30 +0530

    Update AppDelegate.swift

--- bitcoin-master/bitcoin/AppDelegate.swift
+++ bitcoin-master/bitcoin/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift         
 //  bitcoin        
 //           
-//  Created by Filipe Santos Correa on 14.07.17.      
+//  Created by Filipe Santos Correa on 14.07.17.       
 //  Copyright © 2017 Filipe Santos Correa. All rights reserved.  
 //  
 
